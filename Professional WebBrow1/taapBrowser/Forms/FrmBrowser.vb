07b48c7df3e3204cdd2d684ef9a7b0227812dbfe
caillou15 <pierre.douci@gmail.com>
2016-11-13 13:25:27 +0100

    téléchargement des fichiers

---
+++ Professional WebBrow1/taapBrowser/Forms/FrmBrowser.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+
+Imports WeifenLuo.WinFormsUI.Docking
+
+Namespace taapBrowser.Forms
+	Partial Public Class FrmBrowser
+		Inherits DockContent
+
+		Public Sub New()
+			InitializeComponent()
+
+		End Sub
+        Public Shared WithEvents tbSeachBox As Container
+        Private Sub tbSearchBox_Click(sender As Object, e As EventArgs) Handles tbSearchBox.Click
+            Dim Recherche As Integer
+            Recherche = AddressBar
+        End Sub
+    End Class
+End Namespace
